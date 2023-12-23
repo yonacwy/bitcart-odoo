77afdaf92e376d0455881cf4104ed2f13d3394fb
Yona <3211998+yonacwy@users.noreply.github.com>
2023-12-22 22:53:28 -0500

    Add files via upload

--- payment_bitcart/__init__.py
+++ payment_bitcart/__init__.py
@@ -2,10 +2,10 @@ from odoo.addons.payment import reset_payment_provider, setup_provider
 
 from . import controllers, models  # noqa: F401
 
+    
+def post_init_hook(env):
+    setup_provider(env, 'bitcart')
 
-def post_init_hook(cr, registry):
-    setup_provider(cr, registry, "bitcart")
 
-
-def uninstall_hook(cr, registry):
-    reset_payment_provider(cr, registry, "bitcart")
+def uninstall_hook(env):
+    reset_payment_provider(env, 'bitcart')    
