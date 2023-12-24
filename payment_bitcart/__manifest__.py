11c3de801d2a08aed30af83fb8d91f97a7af7395
Yona <3211998+yonacwy@users.noreply.github.com>
2023-12-24 12:29:23 -0500

    Add files via upload

--- payment_bitcart/__manifest__.py
+++ payment_bitcart/__manifest__.py
@@ -5,11 +5,14 @@
     "category": "Accounting/Payment Providers",
     "summary": "Self-hosted, open-source cryptocurrency payment processor and developer solutions",
     "website": "https://bitcart.ai",
-    "depends": ["payment"],
+    "depends": ['payment', 'mail'],
     "data": [
+        "views/main_view.xml",
+        "views/menus_bitcart.xml",
         "views/payment_bitcart_templates.xml",
         "views/payment_provider_views.xml",
         "views/payment_transaction_views.xml",
+        "data/ir_sequence.xml",
         "data/payment_provider_data.xml",
     ],
     "images": ["static/description/banner.png"],
@@ -18,3 +21,4 @@
     "uninstall_hook": "uninstall_hook",
     "license": "LGPL-3",
 }
+
